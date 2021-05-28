7b56451704b03c98b8fea479224836ad99a6c2d6
JinSeungHo <sooooooo8787@naver.com>
2021-05-28 21:02:05 +0900

    210528

---
+++ HTML+CSS/JejuCodingCamp/jejucafe-htmlcss/js/index.js
@@ -0,0 +1,11 @@
+(function() {
+
+    var menuBtn = document.querySelector('.global-menu-btn');
+
+    menuBtn.addEventListener('click', function() {
+        document.body.classList.toggle('menu-on');
+    });
+
+})();
+
+
