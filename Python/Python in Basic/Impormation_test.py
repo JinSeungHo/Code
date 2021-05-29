62542375605caa543796fd2a7293026838d686d2
JinSeungHo <sooooooo8787@naver.com>
2021-05-29 18:56:49 +0900

    최종

--- Python/Python in Basic/Impormation_test.py
+++ Python/Python in Basic/Impormation_test.py
@@ -7,4 +7,4 @@ with open("../data/Impormation.txt", "w", encoding="UTF-8") as File:
         answer = input("답을 입력하세요' : ")
         if answer == "q" or answer == "ㅂ":
             break
-        File.write("{} : {}\n".format(question, answer))
+        File.write("{}:{}\n".format(question, answer))
